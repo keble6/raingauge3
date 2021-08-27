563eea7432d5a43b1e7f572a44bbced3bf4a4a7b
Rob Wilson <rob@rwilson.org.uk>
2021-08-27 21:42:38 +0100

    more code added

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+function getRegister (registerAddress: number) {
+    pins.i2cWriteNumber(
+    deviceAddress,
+    registerAddress,
+    NumberFormat.UInt8BE,
+    true
+    )
+    return pins.i2cReadNumber(registerAddress, NumberFormat.UInt8BE, false)
+}
+function getBit (bitNumber: number, registerAddress: number) {
+    value = getRegister(registerAddress)
+    value = value & (1 << bitNumber)
+return value
+}
 function setRegister (registerAddress: number, value: number) {
     pins.i2cWriteNumber(
     deviceAddress,
@@ -13,7 +27,11 @@ function setRegister (registerAddress: number, value: number) {
     )
 }
 let deviceAddress = 0
+let PU_CTRL = 0
+let CTRL1 = 1
+let CTRL2 = 2
+let OCAL1_B2 = 3
+let OCAL1_B1 = 4
+let OCAL1_B0 = 5
+let value = 0
 deviceAddress = 42
-basic.forever(function () {
-	
-})
