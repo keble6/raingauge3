def0823e41120bb5d86d4f9f9722ab6632d206df
Rob Wilson <rob@rwilson.org.uk>
2021-08-28 21:51:23 +0100

    doing begin()

--- main.ts
+++ main.ts
@@ -7,24 +7,45 @@ function getRegister (registerAddress: number) {
     )
     return pins.i2cReadNumber(registerAddress, NumberFormat.UInt8BE, false)
 }
+function calibrateAFE () {
+    return 0
+}
 function getBit (bitNumber: number, registerAddress: number) {
     value = getRegister(registerAddress)
     value &= (1 << bitNumber)
 return value
 }
+function setGain (gainValue: number) {
+    return 0
+}
+function setLDO (ldoValue: number) {
+    return 0
+}
 function clearBit (bitNumber: number, registerAddress: number) {
     value = getRegister(registerAddress)
     value &= ~(1 << bitNumber)
+return setRegister(registerAddress, value)
 }
 function begin (initialize: boolean) {
     if (isConnected() == 0) {
         if (isConnected() == 0) {
             return 0
         }
-        if (initialize) {
-        	
-        }
+    } else {
+        result = 1
     }
+    if (initialize) {
+        result &= reset()
+result &= powerUp()
+result &= setLDO(1)
+result &= setGain(1)
+result &= setSampleRate(1)
+result &= setRegister(1, 1)
+    }
+    return 0
+}
+function powerUp () {
+    return 0
 }
 function setBit (bitNumber: number, registerAddress: number) {
     value = getRegister(registerAddress)
@@ -32,6 +53,8 @@ function setBit (bitNumber: number, registerAddress: number) {
 }
 function reset () {
     setBit(PU_CTRL_RR, PU_CTRL)
+    basic.pause(1)
+    return clearBit(PU_CTRL_RR, PU_CTRL)
 }
 function setRegister (registerAddress: number, value: number) {
     pins.i2cWriteNumber(
@@ -46,6 +69,10 @@ function setRegister (registerAddress: number, value: number) {
     NumberFormat.UInt8BE,
     false
     )
+    return 1
+}
+function setSampleRate (rate: number) {
+    return 0
 }
 // Test for ACK - dummy for now, casn ubit do this?
 function isConnected () {
@@ -54,15 +81,16 @@ function isConnected () {
 let PU_CTRL_RR = 0
 let deviceAddress = 0
 let PU_CTRL = 0
-PU_CTRL = 0
+let result = 0
 let value = 0
+let CTRL2_CALMOD = 0
+PU_CTRL = 0
 let CTRL1 = 1
 let CTRL1_GAIN = 2
 let CTRL1_VLDO = 5
 let CTRL1_DRDY_SEL = 6
 let CTRL1_CRP = 7
 let CTRL2 = 2
-let CTRL2_CALMOD = 0
 let CTRL2_CALS = 2
 let CTRL2_CAL_ERROR = 3
 let CTRL2_CRS = 4
