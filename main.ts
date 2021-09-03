7efde3e36a4c31a8a642a43231a09bbbfe090b29
Rob Wilson <rob@rwilson.org.uk>
2021-09-03 16:15:30 +0100

    functions ordered

--- main.ts
+++ main.ts
@@ -1,101 +1,13 @@
-function getRegister (registerAddress: number) {
-    // line 384
-    pins.i2cWriteNumber(deviceAddress,registerAddress,
-    NumberFormat.UInt8BE,false)
-    return pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false)
-}
-function getRevisionCode () {
-    // line 228
-    let revisionCode = getRegister(DEVICE_REV)
-    return (revisionCode)
-}
-function calibrateAFE () {
-    // line 86
-    beginCalibrateAFE()
-    return waitForCalibrateAFE(1000)
-}
-function getBit (bitNumber: number, registerAddress: number) {
-    // line 376
-    let value = getRegister(registerAddress)
-    value &= (1 << bitNumber)
-    value = value >>> bitNumber // added because original returns a number, not a boolean!
-return value
-}
-function setGain (gainValue: number) {
-    // line 215
-    if (gainValue > 7) {
-        gainValue = 7
-    }
-    let value = getRegister(CTRL1)
-    value &= 0b11111000
-    value |= gainValue
-return setRegister(CTRL1, value)
-}
-function setZeroOffset (newZeroOffset: number) {
-    // line 299
-    _zeroOffset = newZeroOffset
-}
-// line 199
-function setLDO (ldoValue: number) {
-    if(ldoValue > 0b111){
-        ldoValue = 0b111
-    }
-    let value = getRegister(CTRL1)
-    value &= 0b11000111
-    value |= ldoValue << 3
-    setRegister(CTRL1, value)
-    return setBit(PU_CTRL_AVDDS, PU_CTRL)
-}
-function clearBit (bitNumber: number, registerAddress: number) {
-    // line 368
-    let value = getRegister(registerAddress)
-    value &= ~(1 << bitNumber)
-    return setRegister(registerAddress, value)
-}
-function setIntPolarityHigh () {
-    // line 348
-    return clearBit(CTRL1_CRP, CTRL1)
-}
-function getWeight (allowNegativeWeights: boolean, samplesToTake: number) {
-    // line 330
-    let onScale = getAverage(samplesToTake)
-    if (!(allowNegativeWeights)) {
-        if (onScale < _zeroOffset) {
-            onScale = _zeroOffset
-        }
-    }
-    return (onScale - _zeroOffset) / _calibrationFactor
-}
-function beginCalibrateAFE () {
-    // line 94
-    setBit(CTRL2_CALS, CTRL2)
-}
-//not used - waitForCalibrateAFE now uses direct status call via getBit
-/*function calAFEStatus () {
-    // line 100
-    if (getBit(CTRL2_CALS, CTRL2) == 1) {
-        return CAL_IN_PROGRESS
-    } else if (getBit(CTRL2_CAL_ERROR, CTRL2) == 1) {
-        return CAL_FAILURE
-    } else {
-        return CAL_SUCCESS
-    }
-}*/
-
-function available () {
+function available() {
     //line 77
-    if (getBit(PU_CTRL_CR, PU_CTRL)){
+    if (getBit(PU_CTRL_CR, PU_CTRL)) {
         return true
-    } else{
+    } else {
         return false
     }
-     
+
 }
-function calculateZeroOffset (averageAmount: number) {
-    // line 293
-    setZeroOffset(getAverage(averageAmount))
-}
-function begin (initialize: boolean) {
+function begin(initialize: boolean) {
     //line 30
     if (isConnected() == false) {
         if (isConnected() == false) { //2nd try
@@ -121,33 +33,36 @@ function begin (initialize: boolean) {
         result = result && calibrateAFE()
         serial.writeLine("init8 =" + result)
     }
-    
+
     return result
 }
-function powerUp () {
-    //line 164
-    setBit(PU_CTRL_PUD, PU_CTRL) //power up digital
-    setBit(PU_CTRL_PUA, PU_CTRL) //power up analog
-    //debug - bit 3 should be 1
-    let result=getRegister(PU_CTRL)
-    //serial.writeLine("PU_CTRL =" + result)
-    //result = getBit(PU_CTRL_PUR, PU_CTRL)
-    //serial.writeLine("PU_PUR =" + result)
-    //end debug
-    let counter = 0
-    while (true) {
-        if (getBit(PU_CTRL_PUR, PU_CTRL) == 1) { //read ready bit
-            break
-        }
-        basic.pause(1)
-        counter += 1
-        if (counter > 100) {
-            return false
-        }
-    }
-    return true
+function beginCalibrateAFE() {
+    // line 94
+    setBit(CTRL2_CALS, CTRL2)
 }
-function getAverage (averageAmount: number) {
+function calculateCalibrationFactor(weightOnScale: number, averageAmount: number) {
+    // line 310
+    let onScale = getAverage(averageAmount)
+    let newCalFactor = (onScale - _zeroOffset) / weightOnScale
+    setCalibrationFactor(newCalFactor)
+}
+
+function calculateZeroOffset(averageAmount: number) {
+    // line 293
+    setZeroOffset(getAverage(averageAmount))
+}
+function calibrateAFE() {
+    // line 86
+    beginCalibrateAFE()
+    return waitForCalibrateAFE(1000)
+}
+function clearBit(bitNumber: number, registerAddress: number) {
+    // line 368
+    let value = getRegister(registerAddress)
+    value &= ~(1 << bitNumber)
+    return setRegister(registerAddress, value)
+}
+function getAverage(averageAmount: number) {
     // line 269
     total = 0
     let samplesAquired = 0
@@ -165,14 +80,110 @@ function getAverage (averageAmount: number) {
         basic.pause(1)
     }
     total /= averageAmount
-return total
+    return total
 }
-function setCalibrationFactor (newCalFactor: number) {
+function getBit(bitNumber: number, registerAddress: number) {
+    // line 376
+    let value = getRegister(registerAddress)
+    value &= (1 << bitNumber)
+    value = value >>> bitNumber // added because original returns a number, not a boolean!
+    return value
+}
+function getCalibrationFactor() {
+    // line324
+    return _calibrationFactor
+}
+function getReading() {
+    // Send register address
+    pins.i2cWriteNumber(
+        deviceAddress,
+        ADCO_B2,
+        NumberFormat.UInt8BE,
+        false
+    )
+    valueRaw = pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false) << 16
+    valueRaw |= pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false) << 8
+    valueRaw |= pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false)
+    let valueShifted = valueRaw << 8
+    return valueShifted >> 8
+}
+function getRegister (registerAddress: number) {
+    // line 384
+    pins.i2cWriteNumber(deviceAddress,registerAddress,
+    NumberFormat.UInt8BE,false)
+    return pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false)
+}
+function getRevisionCode () {
+    // line 228
+    let revisionCode = getRegister(DEVICE_REV)
+    return (revisionCode)
+}
+function getWeight(allowNegativeWeights: boolean, samplesToTake: number) {
+    // line 330
+    let onScale = getAverage(samplesToTake)
+    if (!(allowNegativeWeights)) {
+        if (onScale < _zeroOffset) {
+            onScale = _zeroOffset
+        }
+    }
+    return (onScale - _zeroOffset) / _calibrationFactor
+}
+function getZeroOffset() {
+    // line 304
+    return _zeroOffset
+}
+// Test for ACK - dummy for now, can ubit do this?
+function isConnected() {
+    // line 69
+    return true
+}
+function powerDown() {
+    // line 183
+    clearBit(PU_CTRL_PUD, PU_CTRL)
+    return clearBit(PU_CTRL_PUA, PU_CTRL)
+}
+function powerUp() {
+    //line 164
+    setBit(PU_CTRL_PUD, PU_CTRL) //power up digital
+    setBit(PU_CTRL_PUA, PU_CTRL) //power up analog
+    //debug - bit 3 should be 1
+    let result = getRegister(PU_CTRL)
+    //serial.writeLine("PU_CTRL =" + result)
+    //result = getBit(PU_CTRL_PUR, PU_CTRL)
+    //serial.writeLine("PU_PUR =" + result)
+    //end debug
+    let counter = 0
+    while (true) {
+        if (getBit(PU_CTRL_PUR, PU_CTRL) == 1) { //read ready bit
+            break
+        }
+        basic.pause(1)
+        counter += 1
+        if (counter > 100) {
+            return false
+        }
+    }
+    return true
+}
+function reset() {
+    // line 190
+    setBit(PU_CTRL_RR, PU_CTRL)
+    basic.pause(1)
+    return clearBit(PU_CTRL_RR, PU_CTRL)
+}
+function setBit(bitNumber: number, registerAddress: number) {
+    // line 360
+    let value = getRegister(registerAddress)
+    value |= (1 << bitNumber)
+    return setRegister(registerAddress, value)
+}
+
+function setCalibrationFactor(newCalFactor: number) {
     // line 319
     _calibrationFactor = newCalFactor
 }
 // line 155
-function setChannel (channelNumber: number) {
+function setChannel(channelNumber: number) {
     let CHANNEL_1: null = null
     if (channelNumber == CHANNEL_1) {
         // Channel 1 default
@@ -181,77 +192,52 @@ function setChannel (channelNumber: number) {
         return setBit(CTRL2_CHS, CTRL2)
     }
 }
-function getZeroOffset () {
-    // line 304
-    return _zeroOffset
+function setGain (gainValue: number) {
+    // line 215
+    if (gainValue > 7) {
+        gainValue = 7
+    }
+    let value = getRegister(CTRL1)
+    value &= 0b11111000
+    value |= gainValue
+return setRegister(CTRL1, value)
 }
-function getCalibrationFactor () {
-    // line324
-    return _calibrationFactor
+function setIntPolarityHigh() {
+    // line 348
+    return clearBit(CTRL1_CRP, CTRL1)
 }
-function setBit (bitNumber: number, registerAddress: number) {
-    // line 360
-    let value = getRegister(registerAddress)
-    value |= (1 << bitNumber)
-    return setRegister(registerAddress, value)
-}
-function setIntPolarityLow () {
+function setIntPolarityLow() {
     // line 353
     return setBit(CTRL1_CRP, CTRL1)
 }
-function powerDown () {
-    // line 183
-    clearBit(PU_CTRL_PUD, PU_CTRL)
-    return clearBit(PU_CTRL_PUA, PU_CTRL)
+// line 199
+function setLDO(ldoValue: number) {
+    if (ldoValue > 0b111) {
+        ldoValue = 0b111
+    }
+    let value = getRegister(CTRL1)
+    value &= 0b11000111
+    value |= ldoValue << 3
+    setRegister(CTRL1, value)
+    return setBit(PU_CTRL_AVDDS, PU_CTRL)
 }
-function getReading () {
-    // Send register address
-    pins.i2cWriteNumber(
-    deviceAddress,
-    ADCO_B2,
-    NumberFormat.UInt8BE,
-    false
-    )
-    valueRaw = pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false) << 16
-    valueRaw |= pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false) << 8
-    valueRaw |= pins.i2cReadNumber(deviceAddress, NumberFormat.UInt8BE, false)
-    let valueShifted = valueRaw << 8
-    return valueShifted >> 8
-}
-function reset () {
-    // line 190
-    setBit(PU_CTRL_RR, PU_CTRL)
-    basic.pause(1)
-    return clearBit(PU_CTRL_RR, PU_CTRL)
-}
-function setRegister (registerAddress: number, value: number) {
+function setRegister(registerAddress: number, value: number) {
     // line 401
     let buf = pins.createBuffer(2)
     buf[0] = registerAddress
     buf[1] = value
-    pins.i2cWriteBuffer(deviceAddress,buf)
+    pins.i2cWriteBuffer(deviceAddress, buf)
     return true
 }
-function setSampleRate (rate: number) {
+function setSampleRate(rate: number) {
     // line 142
-    if(rate> 0b111){
+    if (rate > 0b111) {
         rate = 0b111
     }
     let value = getRegister(CTRL2)
     value &= 0b10001111 ///clear CRS bits
     value |= rate << 4  //Mask in new CRS bits
-return setRegister(CTRL2, value)
-}
-function calculateCalibrationFactor (weightOnScale: number, averageAmount: number) {
-    // line 310
-    let onScale = getAverage(averageAmount)
-    let newCalFactor = (onScale - _zeroOffset) / weightOnScale
-    setCalibrationFactor(newCalFactor)
-}
-// Test for ACK - dummy for now, can ubit do this?
-function isConnected () {
-    // line 69
-    return true
+    return setRegister(CTRL2, value)
 }
 function waitForCalibrateAFE (timeout_ms: number) {
     // line 119
@@ -348,3 +334,24 @@ serial.writeLine("S T A R T I N G !")
 serial.writeLine("begin result =" + begin(true))
 serial.writeLine("Rev code = " + getRevisionCode())
 serial.writeLine("DEVICE_REV = " + DEVICE_REV)
+
+
+function setZeroOffset(newZeroOffset: number) {
+    // line 299
+    _zeroOffset = newZeroOffset
+}
+
+
+
+
+//not used - waitForCalibrateAFE now uses direct status call via getBit
+/*function calAFEStatus () {
+    // line 100
+    if (getBit(CTRL2_CALS, CTRL2) == 1) {
+        return CAL_IN_PROGRESS
+    } else if (getBit(CTRL2_CAL_ERROR, CTRL2) == 1) {
+        return CAL_FAILURE
+    } else {
+        return CAL_SUCCESS
+    }
+}*/
